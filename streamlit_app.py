7607d62fcd6a62f853ff7813193359b36d00b97e
Olivier Douangvichith <olivier.douangvichith@gmail.com>
2024-05-27 23:10:13 +0200

    Added Date arrivee

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,8 +47,8 @@ def main():
       arrivee_choice = st.selectbox("Arrivée", arrivee_lieu)
 
       today = datetime.now()
-      tomorrow = today + timedelta(days=1) # datetime.date(today.year, today.month, today+1)
-      st.write("tomorrow: ", tomorrow)
+      tomorrow = today + timedelta(days=1)
+      # st.write("tomorrow: ", tomorrow)
       arrivee_date = st.date_input("Date Arrivée", value=None, min_value=tomorrow, format="DD/MM/YYYY")
 
       duree_jour = st.number_input("Durée de visite", min_value=1, max_value=None, value="min", placeholder="En jour...")
