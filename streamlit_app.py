4cc5f96103a08165acac766d3f1e6f91f3ce0368
Olivier Douangvichith <olivier.douangvichith@gmail.com>
2024-05-27 22:48:07 +0200

    Added Date arrivee

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ def main():
 
       arrivee_date = st.date_input("Date Arrivée", value=None)
 
-      duree_jour = st.number_input("Durée de visite", value="int", placeholder="En jour...")
+      duree_jour = st.number_input("Durée de visite", value=None, placeholder="En jour...")
 
 
       # Paramètres de la base de données
